4afaba2b22047e4d705c21fc5a3d4ac8e08d7df2
Min Hoo Lee <minhoo2000@gmail.com>
2017-07-31 12:35:06 -0700

    Fix VBox Guest Additions Compatibility
    Installing the virtualbox guest additions extension pack inside the VM causes compatibility issues if you have a later version of virtualbox installed on the host OS. The plugin vagrant-vbguest is unable to update the guest additions automatically.

--- setup_vbox_guest_additions.sh
+++ setup_vbox_guest_additions.sh
@@ -8,8 +8,3 @@ export DEBIAN_FRONTEND=noninteractive
 # Install the kernel sources before the guest additions to guarantee that
 # we can compile the kernel module.
 apt-get install -q -y linux-headers-amd64
-
-# Now we can install the guest additions.
-apt-get install -q -y \
-    virtualbox-guest-dkms \
-    virtualbox-guest-x11
